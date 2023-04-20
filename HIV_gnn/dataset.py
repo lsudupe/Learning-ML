69d3e978a780d03d1867d8a5e98411cc5885eaaa
lsudupe <sudupelaura@gmail.com>
2023-04-20 13:31:24 +0300

    small changes

--- HIV_gnn/dataset.py
+++ HIV_gnn/dataset.py
@@ -1,39 +1,17 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-
-# Load the data
-DATA_PATH = "./HIV_gnn/data/HIV.csv"
-data = pd.read_csv(DATA_PATH)
-data.head()
-
-# Check the data
-print(data.shape)
-print(data["HIV_active"].value_counts())
-# the values are not balance, we need to consider this latter
-
-# Visualize molecules
-import rdkit
-from rdkit import Chem
-from rdkit.Chem import Draw
-import matplotlib.pyplot as plt
-
-samples_smiles = data["smiles"][4:30].values
-sample_moles = [Chem.MolFromSmiles(smiles) for smiles in samples_smiles]
-grid = Draw.MolsToGridImage(sample_moles,
-                            molsPerRow =4,
-                            subImgSize =(200,200))
-plt.imshow(grid)
-
-
-# check our packages versions
 import torch
 import torch_geometric
-from torch_geometric.data import Data
+from torch_geometric.data import Dataset
+import numpy as np
+import os
 from tqdm import tqdm
+import deepchem as dc
+from rdkit import Chem
 
-print(f"torch version/t/t: {torch.__version__}")
-print(f"cuda available: {torch.cuda.is_available()}")
-print(f"torch geometric version: {torch_geometric.__version__}")
+print(f"Torch version: {torch.__version__}")
+print(f"Cuda available: {torch.cuda.is_available()}")
+print(f"Torch geometric version: {torch_geometric.__version__}")
+
 
 class MoleculeDataset(Dataset):
     def __init__(self, root, filename, test=False, transform=None, pre_transform=None):
@@ -41,90 +19,78 @@ class MoleculeDataset(Dataset):
         root = Where the dataset should be stored. This folder is split
         into raw_dir (downloaded dataset) and processed_dir (processed data).
         """
-
+        self.test = test
+        self.filename = filename
         super(MoleculeDataset, self).__init__(root, transform, pre_transform)
 
     @property
     def raw_file_names(self):
-        ''''''
-        return HIV.csv
+        """ If this file exists in raw_dir, the download is not triggered.
+            (The download func. is not implemented here)
+        """
+        return self.filename
 
     @property
     def processed_file_names(self):
+        """ If these files are found in raw_dir, processing is skipped"""
+        self.data = pd.read_csv(self.raw_paths[0]).reset_index()
 
-    def dowloaded(self):
+        if self.test:
+            return [f'data_test_{i}.pt' for i in list(self.data.index)]
+        else:
+            return [f'data_{i}.pt' for i in list(self.data.index)]
+
+    def download(self):
         pass
 
     def process(self):
-        self.data = pd.read_csv(self.raw_paths[0])
-        for index, mol in tqdm(self.data.iterrows(), total=self.data.shape[0]):
-            mol_obj = Chem.MolFromSmiles(mol["smiles"])
-            # get node features
-            node_feats = self.get_node_features(mol_obj)
-            # get edge features
-            edge_feats = self.get_edge_features(mol_obj)
-            # get adjacency info
-            edge_index = self.get_adjacency_info(mol_obj)
-            # get labels info
-            label = self.get_labels()
+        self.data = pd.read_csv(self.raw_paths[0]).reset_index()
+        featurizer = dc.feat.MolGraphConvFeaturizer(use_edges=True)
+        for index, row in tqdm(self.data.iterrows(), total=self.data.shape[0]):
+            # Featurize molecule
+            mol = Chem.MolFromSmiles(row["smiles"])
+            f = featurizer._featurize(mol)
+            data = f.to_pyg_graph()
+            data.y = self._get_label(row["HIV_active"])
+            data.smiles = row["smiles"]
+            if self.test:
+                torch.save(data,
+                           os.path.join(self.processed_dir,
+                                        f'data_test_{index}.pt'))
+            else:
+                torch.save(data,
+                           os.path.join(self.processed_dir,
+                                        f'data_{index}.pt'))
 
-            # Create data object
-            data = Data(x = node_feats,
-                        edge_index = edge_index,
-                        edge_attr = edge_feats,
-                        y= label,
-                        smiles = mol["smiles"]
-                        )
+    def _get_label(self, label):
+        label = np.asarray([label])
+        return torch.tensor(label, dtype=torch.int64)
 
-    def get_node_features(self):
-    '''This will return a matrix /2d array of shape
-    [number of nodes, node feature size]'''
-        all_node_features = []
-        for atom in mol.GetAtoms():
-            node_feats = []
-            # Feature 1: Atomic number
-            node_feats.append(atom.GetAtomicNum())
-            # Feature 2: Atom degree
-            node_feats.append(atom.GetDegree())
-            # Feature 3: Formal charge
-            node_feats.append(atom.GetFormalCharge())
-            # Feature 4: Hybridization
-            node_feats.append(atom.GetHybridization())
-            # Feature 5: Aromaticity
-            node_feats.append(atom.GetIsAromatic())
+    def len(self):
+        return self.data.shape[0]
 
-        # append node features to matrix
-        all_node_features.append(node_feats)
-
-        all_node_features = np.sarray(all_node_features)
-        return torch.tensor(all_node_features, dtype=torch.float)
-
-    def get_edge_features(self, mol):
-        all_bond_features = []
-        for edge in mol.GetBonds():
-            edge_feats = []
-            edge_feats.append(e)
-
-    def get_adjacency_info(self, mol):
-
-    def get_labels(self):
+    def get(self, idx):
+        """ - Equivalent to __getitem__ in pytorch
+            - Is not needed for PyG's InMemoryDataset
+        """
+        if self.test:
+            data = torch.load(os.path.join(self.processed_dir,
+                                           f'data_test_{idx}.pt'))
+        else:
+            data = torch.load(os.path.join(self.processed_dir,
+                                           f'data_{idx}.pt'))
+        return data
 
 
-for index, mol in tqdm(data.iterrows(), total=data.shape[0]):
-    a = index
-    b = mol
 
-type(mol_obj)
-node_feats = get_node_features(mol_obj)
+# Test the dataset
+dataset = MoleculeDataset(root="data/")
 
-molecule_class = rdkit.Chem.rdchem.Mol
-molecule_class
-print(dir(molecule_class))
+print(dataset[0].edge_index.t())
+print(dataset[0].x)
+print(dataset[0].edge_attr)
+print(dataset[0].y)
 
-a =data.iterrows()
-a.head()
-
-mol["HIV_active"]
 
 
 
